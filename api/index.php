3c1955547ab39e7617e46c0b0669c8663ab4a610
nihad1213 <nihad.nemetli@gmail.com>
2025-04-20 17:52:07 +0400

    some tests in index.php file

--- api/index.php
+++ api/index.php
@@ -1,3 +1,14 @@
 <?php
 
-echo "test";
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+$parts = explode("/", $path);
+
+$resource = $parts[2];
+
+$id = $parts[3] ?? null;
+
+echo $resource . " " . $id; 
+
+// print_r($parts);
+// echo "test";
