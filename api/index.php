c9ffd601a9bfce47fd00bea4b5a070d96cc56e29
Nihad <nihad.nemetli@gmail.com>
2025-04-21 10:38:48 +0400

    small fixes

--- api/index.php
+++ api/index.php
@@ -37,7 +37,7 @@ if ($path === "tasks" || str_starts_with($path, "tasks/")) {
 
         if ($type === 'Bearer' && !empty($token)) {
             try {
-                // ✅ Check if JWT_SECRET is set
+                //Check if JWT_SECRET is set
                 $jwtSecret = $_ENV["JWT_SECRET"] ?? null;
                 if (!$jwtSecret) {
                     http_response_code(500);
